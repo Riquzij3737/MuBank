c8bd8c8046a9eca72b90be1c0b1fae33c3655cde
riquzin077 <henriquemaurel37@gmail.com>
2025-07-21 22:04:09 -0300

    Criei um serviço que usa a api de gemini para requisições http

---
+++ Services/GeminiService.cs
@@ -0,0 +1,38 @@
+﻿using Mubank.Models;
+using System.Text.Json;
+using System.Text;
+
+namespace Mubank.Services
+{
+    public class GeminiService
+    {
+        public async Task<string> SendHttpPost()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string BodyContent = JsonSerializer.Serialize<GeminiModel>(new GeminiModel()
+                {
+                    raiz = new Root()
+                    {
+                        Contents = new List<Content>()
+                        {
+                            new Content()
+                            {
+                                Parts = new List<Part>()
+                                {
+                                    new Part() { Text = "Gere uma frase biblica para alegrar o dia de uma pessoa de no maximo 30 linhas" }
+                                }
+                            }
+                        }
+                    }
+                });
+                var body = new StringContent(BodyContent, Encoding.UTF8, "application/json");
+
+
+                var Result = await client.PostAsync("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCWbTec8TIwOEFdWkq5FyNsLvRGM2PO58Y",body);
+
+                return await Result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
